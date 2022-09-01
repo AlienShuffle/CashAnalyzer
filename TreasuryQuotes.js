9544cffd63c66d68536fef73bf8a53e7e80e2f16
Readington Dude <readngtndude@gmail.com>
2022-08-31 21:35:55 -0400

    edits

--- TreasuryQuotes.js
+++ TreasuryQuotes.js
@@ -1,16 +1,22 @@
 // testing This is the Node version that will work locally
 const puppeteer = require("puppeteer");
 
-async function getTreasuryQuotes() {
-    const browser = await puppeteer.launch({
-        defaultViewport: null,
-        headless: false,  // comment out to make this run headless for production.
-        ignoreDefaultArgs: ['--disable-extensions'],
-        //args: ['--window-size=1920,1080']
-        args: ['--window-size=800,600']
-    });
+// shared global browser instance.
+var browser = null;
 
-    // open an instance of Chromium.
+async function getTreasuryQuotes() {
+    if (!browser) { 
+        browser = await puppeteer.launch({
+            defaultViewport: null,
+            //headless: false,  // comment out to make this run headless for production.
+            ignoreDefaultArgs: ['--disable-extensions'],
+            //args: ['--window-size=1920,1080']
+            args: ['--window-size=800,600']
+        });
+        console.log('browser started');
+    }
+
+    // open an  new page instance of Chromium.
     const page = await browser.newPage();
 
     // browse to the marketable securities page
@@ -54,33 +60,32 @@ async function getTreasuryQuotes() {
         3
         const rawHeader = await page.evaluate(input => input.innerHTML, cols[col]);
         headers[col] = rawHeader.toLowerCase().replace(/ /gi, '');
-        console.log(headers[col]);
+        //console.log(headers[col]);
     }
 
     // This parses the rate table and puts in an array for conversion to JSON.
     let data = [];
     for (let row = 1; row < rows.length; row++) {
-        console.log('row[' + row + '] = ' + await page.evaluate(input => input.innerHTML, rows[row]));
+        //console.log('row[' + row + '] = ' + await page.evaluate(input => input.innerHTML, rows[row]));
         // get an array of all the columns for the current row.
         cols = await rows[row].$$('td');
-        console.log('cols = ' + cols.length);
+        //console.log('cols = ' + cols.length);
 
         if (cols.length == 8) {
             var rowData = {};
             for (let col = 0; col < cols.length; col++) {
                 rowData[headers[col]] = await page.evaluate(input => input.innerHTML, cols[col]);
-                console.log('rowData[' + headers[col] + '] = ' + rowData[headers[col]]);
+                //console.log('rowData[' + headers[col] + '] = ' + rowData[headers[col]]);
             }
             data.push(rowData);
         }
-
     }
     console.log('data Rows = ' + data.length);
     console.log(JSON.stringify(data));
     await inputHandle.dispose();
     await page.close();
     //await browser.close();
-    console.log('closed');
+    console.log('page closed');
 }
 // For a local node run, this is the test function.
-getTreasuryQuotes();
+//getTreasuryQuotes();
