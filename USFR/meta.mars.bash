d88fb5618e8ee7e4c5eb2e85e923ee819e77d1eb
AlienShuffle <readngtndude@gmail.com>
2024-09-05 22:46:45 -0400

    mars updates

---
+++ USFR/meta.mars.bash
@@ -0,0 +1,4 @@
+# this is an include file for platform 
+cvsNew=usfr-distro-new.cvs
+cvsPub='/mnt/c/Users/alan/My Drive (readngtndude@gmail.com)/CashAnalyzer/USFR/USFR-distros.csv'
+#cvsPub='/mnt/h/My Drive/CashAnalyzer/USFR/USFR-distros.csv'
