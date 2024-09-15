f482756abb85f81a460baf66357cad884caebacb
AlienShuffle <readngtndude@gmail.com>
2024-09-15 16:34:16 -0400

    logging update

--- job-control.bash
+++ job-control.bash
@@ -2,6 +2,8 @@
 # Find all the scripts in each system sub-folder with prefix job- and run them with a 6 hour delay
 (while true
 do
+  [ -d $HOME/CashAnalyzer/log ] || mkdir $HOME/CashAnalyzer/log
+  logrotate --state $HOME/CashAnalyzer/log/status $HOME/CashAnalyzer/cashanalyzer.log.conf #--verbose
   for script in `ls */job-*.bash`
   do
     echo ------------
