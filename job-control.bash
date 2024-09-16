024f9cb4218fb027d049d1c9b9a32090defefc32
AlienShuffle <readngtndude@gmail.com>
2024-09-16 14:20:08 -0400

    improved logging

--- job-control.bash
+++ job-control.bash
@@ -1,19 +1,21 @@
 #!/usr/bin/bash
 # Find all the scripts in each system sub-folder with prefix job- and run them with a 6 hour delay
-(while true
+while true
 do
   [ -d $HOME/CashAnalyzer/log ] || mkdir $HOME/CashAnalyzer/log
-  logrotate --state $HOME/CashAnalyzer/log/status $HOME/CashAnalyzer/cashanalyzer.log.conf #--verbose
-  for script in `ls */job-*.bash`
-  do
-    echo ------------
-    echo "Running $script @ `date`"
-    dir=`dirname $script`
-    scriptFile=`basename $script`
-    (cd $dir; eval ./$scriptFile)
-    echo
-  done
-  echo "sleep 6 hours @ `date`"
-  sleep 6h
-done) | tee log/cash-analyzer-jobs.log
+  logrotate --state $HOME/CashAnalyzer/log/status $HOME/CashAnalyzer/cashanalyzer.log.conf | tee log/cash-analyzer-jobs.log
+  (
+    for script in `ls */job-*.bash`
+    do
+      echo ------------
+      echo "Running $script @ `date`"
+      dir=`dirname $script`
+      scriptFile=`basename $script`
+      (cd $dir; eval ./$scriptFile)
+      echo
+    done
+    echo "sleep 6 hours @ `date`"
+    sleep 6h
+  ) | tee log/cash-analyzer-jobs.log
+done
 exit 0
