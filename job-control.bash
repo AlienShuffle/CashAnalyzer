6f4a880ebd4b088c8b2a4438e9717956c9aef024
AlienShuffle <readngtndude@gmail.com>
2024-09-17 19:18:42 -0400

    removed logging from logrotate!

--- job-control.bash
+++ job-control.bash
@@ -3,7 +3,7 @@
 while true
 do
   [ -d $HOME/CashAnalyzer/log ] || mkdir $HOME/CashAnalyzer/log
-  logrotate --state $HOME/CashAnalyzer/log/status $HOME/CashAnalyzer/cashanalyzer.log.conf | tee -a log/cash-analyzer-jobs.log
+  logrotate --state $HOME/CashAnalyzer/log/status $HOME/CashAnalyzer/cashanalyzer.log.conf #| tee -a log/cash-analyzer-jobs.log
   (
     for script in `ls */job-*.bash`
     do
