faa751a2f7ad3e6c8e766dbeb76bcc096c72f77d
AlienShuffle <readngtndude@gmail.com>
2024-09-25 22:49:39 -0400

    pretty formatting updates.

--- USFR/job-USFR-distro-update-json-v97.sh
+++ USFR/job-USFR-distro-update-json-v97.sh
@@ -1,4 +1,4 @@
-source ../meta.`hostname`.sh
+source ../meta.$(hostname).sh
 jsonNew=USFR-distro-new-v97.json
 jsonPub="$publishHome/USFR/USFR-distros-v97.json"
 #echo jsonNew=$jsonNew
@@ -8,26 +8,23 @@ jsonPub="$publishHome/USFR/USFR-distros-v97.json"
 # this script was used in All Funds version 9 (sheets using fintools v97), and can be retired when v9 is completely retired.
 #
 #echo "Starting node retrieval app."
-node ./node-USFR-distro-json-v97.js | jq -S . > "$jsonNew"
-if [ ! $? ]
-then
+node ./node-USFR-distro-json-v97.js | jq -S . >"$jsonNew"
+if [ ! $? ]; then
   echo "USFR distro retrieval failed, exiting."
   exit 1
 fi
-lenNew=`grep -o returnOfCapital "$jsonNew" | wc -l`
-if [ -f "$jsonPub" ]
-then
-  lenPub=`grep -o returnOfCapital "$jsonPub" | wc -l`
+lenNew=$(grep -o returnOfCapital "$jsonNew" | wc -l)
+if [ -f "$jsonPub" ]; then
+  lenPub=$(grep -o returnOfCapital "$jsonPub" | wc -l)
 else
   lenPub=0
 fi
 echo "entries new($lenNew) :: pub($lenPub)"
-if [ $lenNew -le $lenPub ]
-then
+if [ $lenNew -le $lenPub ]; then
   echo 'new distro file is not longer than published file, exiting.'
 else
   # I am trying cat instead of cp because Google Drive sometimes makes a (1) copy of the file.
-  cat "$jsonNew" > "$jsonPub"
+  cat "$jsonNew" >"$jsonPub"
   echo "published (cat>) updated USFR distro history file."
 fi
 exit 0
