688ed581e44c1f7565a5fc89b3a4559f0b98ec55
AlienShuffle <readngtndude@gmail.com>
2024-09-26 15:59:01 -0400

    more defensive coding.

--- Ally/job-Ally-rates-update-v98.sh
+++ Ally/job-Ally-rates-update-v98.sh
@@ -11,7 +11,15 @@ if [ ! $? ]; then
   echo "Ally facts retrieval failed, exiting."
   exit 1
 fi
+if [ ! -s "$jsonNew" ]; then
+  echo "Empty Ally facts file."
+  exit 1
+fi
 dateNew=$(grep asOfDate "$jsonNew" | cut -d: -f2 | sed 's/\"//g' | sed 's/,//g' | sed 's/ //g')
+if [ -z "$dateNew" ]; then
+  echo "New Ally facts file does not include dates."
+  exit 1
+fi
 echo dateNew=$dateNew
 datePub=$(grep asOfDate "$jsonPub" | cut -d: -f2 | sed 's/\"//g' | sed 's/,//g' | sed 's/ //g')
 echo datePub=$datePub
