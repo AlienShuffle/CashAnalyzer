5848a252aaa2f2bb18e530cdd482148762c86a26
AlienShuffle <readngtndude@gmail.com>
2024-09-26 15:48:45 -0400

    make more resilient to time-outs.

--- Ally/node-Ally-facts-v98.js
+++ Ally/node-Ally-facts-v98.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer');
 
-// shared global browser instance.
+// global browser instance.
 let browserPromise = puppeteer.launch({
     defaultViewport: null,
     headless: false,  // comment out to make this run headless for production.
@@ -8,42 +8,38 @@ let browserPromise = puppeteer.launch({
     //args: ['--window-size=800,600', '--no-sandbox']
     args: ['--window-size=800,600']
 });
+
 function run() {
     return new Promise(async (resolve, reject) => {
+        const browser = await browserPromise;
         try {
-            const browser = await browserPromise;
             const page = await browser.newPage();
 
-            // Go for the Savings Rate
-            await page.goto('https://www.ally.com/bank/online-savings-account/');
-            // this page is slow, let's wait.
-            await page.waitForNetworkIdle({
-                idleTime: 1000,
-            });
-            // make sure the page has rendered at least to the ER section.
-            await page.waitForSelector('span.allysf-rates-v1-value');
-
-            // parse out savings Rate
-            // document.querySelector("#fund-nav > div > div:nth-child(1) > table > tbody > tr.strong > td:nth-child(2) > span")
-            let savingsRate = await page.evaluate(() => {
-                const item = document.querySelector("span.allysf-rates-v1-value");
-                return (item) ? item.innerText : '';
-            });
-
-            await page.goto('https://www.ally.com/bank/no-penalty-cd/');
-            // this page is slow, let's wait.
-            await page.waitForNetworkIdle({
-                idleTime: 1000,
-            });
-            // make sure the page has rendered at least to the ER section.
-            await page.waitForSelector('span.allysf-rates-v1-value');
-
-            // parse out savings Rate
-            // document.querySelector("#fund-nav > div > div:nth-child(1) > table > tbody > tr.strong > td:nth-child(2) > span")
-            let npcdRate = await page.evaluate(() => {
-                const item = document.querySelector("span.allysf-rates-v1-value");
-                return (item) ? item.innerText : '';
-            });
+            // so far Ally pages are structured the same, so we can re-use logic just different URLs.
+            async function retrieveAPY(url) {
+                try {
+                    // Go for the Savings Rate
+                    await page.goto(url);
+                    // this page is slow, let's wait.
+                    await page.waitForNetworkIdle({
+                        idleTime: 1000,
+                    });
+                    // make sure the page has rendered at least to the rates section.
+                    await page.waitForSelector('span.allysf-rates-v1-value');
+                } catch (error) {
+                    if (browser) await browser.close();
+                    console.error(((error.name === 'TimeoutError') ? 'Browser timeout occurred' : 'An error occured') + ': ' + url, error);
+                    return reject(error);
+                }
+                // parse out savings Rate
+                const apy = await page.evaluate(() => {
+                    const item = document.querySelector('span.allysf-rates-v1-value');
+                    return (item) ? item.innerText : '';
+                });
+                return apy;
+            }
+            const savingsRate = await retrieveAPY('https://www.ally.com/bank/online-savings-account/');
+            const npcdRate = await retrieveAPY('https://www.ally.com/bank/no-penalty-cd/');
 
             // format return JSON message.
             const now = new Date;
@@ -58,14 +54,15 @@ function run() {
                     accountType: 'NPCD',
                     apy: (npcdRate) ? npcdRate / 100 : 'n/a',
                     asOfDate: asOfDate,
-                }
+                },
             ];
 
             browser.close();
             return resolve(JSON.stringify(facts));
-        } catch (e) {
-            if (!(typeof browser === 'undefined')) browser.close();
-            return reject(e);
+        } catch (error) {
+            if (browser) await browser.close();
+            console.error('Some Error occured', error);
+            return reject(error);
         }
     });
 }
