4c503a557d4f1bf0039b5a9f564a5858950a28b5
AlienShuffle <alan@gebele.com>
2024-10-12 19:57:15 -0400

    install cleanup

--- setupNode.sh
+++ setupNode.sh
@@ -2,24 +2,32 @@
 #
 # One Time Setup Tasks.
 #
-echo If you need to update NVM, do the following:s
-echo steps to update NVM:
-echo "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash"
-echo "steps to get current long-term support (--lts) Node Version"
+#echo If you need to update NVM, do the following:s
+#echo steps to update NVM:
+#echo "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash"
+#echo "steps to get current long-term support (--lts) Node Version"
+echo "##### Get latest Node version"
 echo "nvm version"
 echo "nvm install --lts"
+[ -s "$HOME/.nvm/nvm.sh" ] && \. "$HOME/.nvm/nvm.sh" 
+nvm version
+nvm install --lts
 echo
-echo "install jq (this gives v1.6 currently on WSL)"
+echo "##### jq"
+echo "Make sure jq is installed"
+echo sudo apt install jq
 sudo apt install jq
 
 #
 # Updates to the NPM/Node Environment This needs to be done in each directory
 #
-for dir in Ally USFR; do
+for dir in Ally USFR Vanguard yieldFinder; do
     (
         cd $dir
-        echo "we are going to update npm and the node libraries"
+        echo "##### $dir"
+        echo "Update npm and the node libraries"
         npm install -g npm
         npm install --lts
+        echo
     )
 done
