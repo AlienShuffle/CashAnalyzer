e7a33bbb37a704682ba00a875f4db051aa1b75a3
AlienShuffle <readngtndude@gmail.com>
2024-10-14 20:09:47 -0400

    simplify logic

--- Vanguard/node-Vanguard-update.js
+++ Vanguard/node-Vanguard-update.js
@@ -15,64 +15,29 @@ function run() {
         try {
             const page = await browser.newPage();
 
-            // so far pages are structured the same, so we can re-use logic just different URLs.
-            async function retrieveCashPlusAPY(url) {
+            // so far pages are structured the same, so we can re-use logic just different URLs, and path.
+            async function retrieveAPY(url, path) {
                 try {
-                    // Go for the Savings Rate
                     await page.goto(url);
                     // this page is slow, let's wait.
                     //await page.waitForNetworkIdle({
                     //    idleTime: 1000,
                     //});
                     // make sure the page has rendered at least to the rates section.
-                    await page.waitForSelector("#richtext-3a8a82f700 > p:nth-child(1) > span");
+                    await page.waitForSelector(path);
                 } catch (error) {
                     if (browser) await browser.close();
                     console.error(((error.name === 'TimeoutError') ? 'Browser timeout occurred' : 'An error occured') + ': ' + url, error);
                     return reject(error);
                 }
                 // parse out savings Rate
-                const apy = await page.evaluate(() => {
-                    const item = document.querySelector("#richtext-3a8a82f700 > p:nth-child(1) > span");
-                    if (item) {
-                        const rate = item.innerText.replace(/%/, '');
-                        return rate;
-                    }
-                    return '';
-                });
-                return apy;
+                let inputHandle = await page.$(path);
+                const apyString = await page.evaluate(input => input.innerText, inputHandle);
+                return apyString.replace(/%/, '');
             }
-            const cashPlusYield = await retrieveCashPlusAPY('https://investor.vanguard.com/accounts-plans/vanguard-cash-plus-account');
+            const cashPlusYield = await retrieveAPY('https://investor.vanguard.com/accounts-plans/vanguard-cash-plus-account', "#richtext-3a8a82f700 > p:nth-child(1) > span");
+            const cashDepositYield = await retrieveAPY('https://investor.vanguard.com/investment-products/vanguard-cash-deposit', "#richtext-fb6988b911 > p:nth-child(1) > span");
 
-            //document.querySelector("#richtext-fb6988b911 > p:nth-child(1) > span")
-            async function retrieveCashDepositAPY(url) {
-                try {
-                    // Go for the Savings Rate
-                    await page.goto(url);
-                    // this page is slow, let's wait.
-                    //await page.waitForNetworkIdle({
-                    //    idleTime: 1000,
-                    //});
-                    // make sure the page has rendered at least to the rates section.
-                    await page.waitForSelector("#richtext-fb6988b911 > p:nth-child(1) > span");
-                } catch (error) {
-                    if (browser) await browser.close();
-                    console.error(((error.name === 'TimeoutError') ? 'Browser timeout occurred' : 'An error occured') + ': ' + url, error);
-                    return reject(error);
-                }
-                // parse out savings Rate
-                const apy = await page.evaluate(() => {
-                    const item = document.querySelector("#richtext-fb6988b911 > p:nth-child(1) > span");
-                    if (item) {
-                        const rate = item.innerText.replace(/%/, '');
-                        return rate;
-                    }
-                    return '';
-                });
-                return apy;
-            }
-            const cashDepositYield = await retrieveCashDepositAPY('https://investor.vanguard.com/investment-products/vanguard-cash-deposit');
-            
             // format return JSON message.
             const now = new Date;
             const asOfDate = now.getFullYear() + '-' + (now.getMonth() + 1 + '').padStart(2, '0') + '-' + (now.getDate() + '').padStart(2, '0');
