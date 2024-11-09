696d31beec6fbece345c33445a8b7108fdef521f
AlienShuffle <readngtndude@gmail.com>
2024-11-08 23:38:15 -0500

    cleanup

--- job-control.sh
+++ job-control.sh
@@ -3,7 +3,7 @@ cd $HOME/CashAnalyzer
 [ -d $HOME/CashAnalyzer/log ] || mkdir $HOME/CashAnalyzer/log
 # Find all the scripts in each system sub-folder with prefix job- and run them with a 6 hour delay
 while true; do
-  logrotate --state $HOME/CashAnalyzer/log/status $HOME/CashAnalyzer/cashanalyzer.log.conf #| tee -a log/cash-analyzer-jobs.log
+  logrotate --state $HOME/CashAnalyzer/log/status $HOME/CashAnalyzer/cashanalyzer.log.conf
   (
     for script in $(ls */job-*.sh); do
       echo ------------
