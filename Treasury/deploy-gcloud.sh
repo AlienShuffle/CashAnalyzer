ac7d0c42a6e606f83c39d568ef267e01d4f330b6
AlienShuffle <readngtndude@gmail.com>
2024-11-19 20:56:25 -0500

    PaPower cleanup and FedInvest Prices first run

--- Treasury/deploy-gcloud.sh
+++
@@ -1,7 +0,0 @@
-gcloud functions deploy 'gettreasuryquotes' \
---runtime=nodejs16 \
---region='us-central1' \
---source=. \
---entry-point='getTreasuryQuotes' \
---trigger-http \
---allow-unauthenticated
