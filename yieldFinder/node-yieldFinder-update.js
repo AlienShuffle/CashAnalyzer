f07bbfa2b50dcc53da05cb3710e2c66367375640
AlienShuffle <readngtndude@gmail.com>
2025-03-01 19:37:28 -0500

    cleanup

--- yieldFinder/node-yieldFinder-update.js
+++ yieldFinder/node-yieldFinder-update.js
@@ -1,22 +1,15 @@
 // Work on POSIX and Windows
 const fs = require("fs");
-//const du = require('../lib/dateUtils.js');
 
 // read in from stdin, the yieldFinder.app source data json file.
 const stdinBuffer = fs.readFileSync(process.stdin.fd);
 const json = JSON.parse(stdinBuffer);
 const timestamp = new Date();
-// grab just the bank deposits report, I have the other data from other sources.
-//const savingsAccounts = json.savingsAccounts;
 // convert the data into my facts file format and dump on stdout.
 let resp = [];
-//console.log('json.length = ' + json.length);
 for (let i = 0; i < json.length; i++) {
     const acct = json[i];
-    //console.log('acct.name = ' + acct.name);
-
     if (acct.account_type == 'Savings Account') {
-        //console.log('acct.account_type == Savings Account');
         const row = {
             "source": 'node-yieldFinder-json-update.js',
             "timestamp": timestamp,
