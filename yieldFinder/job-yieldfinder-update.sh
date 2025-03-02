6d63d4cb81dc431c557b29a16f773a03609f9319
AlienShuffle <readngtndude@gmail.com>
2025-03-01 19:22:24 -0500

    date stuff and fix for yieldFinder

--- yieldFinder/job-yieldfinder-update.sh
+++ yieldFinder/job-yieldfinder-update.sh
@@ -1,25 +1,11 @@
 #!/usr/bin/bash
-queryFile=yieldFinder-results.html
+queryFile=yieldFinder-file.json
 jsonFile=yieldFinder-results.json
 
 # go pull the home page off the yieldFinder app.
-curl -sSL https://yieldFinder.app >"$queryFile"
+curl -sSL https://yieldFinder.app/json >"$queryFile"
 if [ ! -s "$queryFile" ]; then
     echo "Empty $queryFile file."
     exit 1
 fi
-asOfDate=$(grep '<span>Rates as of ' "$queryFile" |
-    sed -e 's/ *<span>Rates as of *//' |
-    sed -e 's/ *\/\/ .*<\/span> *//')
-if [ -z "$asOfDate" ]; then
-    echo "Empty asOfDate query."
-    exit 1
-fi
-tr -d '\n' <"$queryFile" |
-    sed -e 's/^.*const jsData = {/{/' |
-    sed -e 's/};.*$/}/' >"$jsonFile"
-if [ ! -s "$jsonFile" ]; then
-    echo "Empty $jsonFile file."
-    exit 1
-fi
-../lib/Bank-update-common-job.sh -b $(basename $(pwd)) -stdin "$jsonFile" -nodearg "$asOfDate" -pubdelay 20 -rundelay 4 "$@"
+../lib/Bank-update-common-job.sh -b $(basename $(pwd)) -stdin "$queryFile" -pubdelay 20 -rundelay 4 "$@"
--- yieldFinder/node-yieldFinder-json-update.js
+++ yieldFinder/node-yieldFinder-json-update.js
@@ -1,25 +1,21 @@
 // Work on POSIX and Windows
 const fs = require("fs");
-//const du = require('../lib/dateUtils.js');
+const du = require('../lib/dateUtils.js');
 
-// read in from stdin, the yieldFinder.app source data json file.
+// read in from stdin, the yieldFinder.app source data.
 const stdinBuffer = fs.readFileSync(process.stdin.fd);
 const json = JSON.parse(stdinBuffer);
-const timestamp = new Date();
+// parse the asOfDate found in argument 2 to the node call into YYY-MM-DD format.
+const asOfDateYYYMMDD = du.getISOString(new Date());
 // grab just the bank deposits report, I have the other data from other sources.
 //const savingsAccounts = json.savingsAccounts;
 // convert the data into my facts file format and dump on stdout.
 let resp = [];
-//console.log('json.length = ' + json.length);
-for (let i = 0; i < json.length; i++) {
-    const acct = json[i];
-    //console.log('acct.name = ' + acct.name);
-
+for (let acct in json) {
     if (acct.account_type == 'Savings Account') {
-        //console.log('acct.account_type == Savings Account');
         const row = {
             "source": 'node-yieldFinder-json-update.js',
-            "timestamp": timestamp,
+            "timestamp": asOfDateYYYMMDD,
             "accountType": acct.name,
             "apy": (acct.rate / 100).toFixed(4) * 1,
             "asOfDate": acct.rate_as_of,
