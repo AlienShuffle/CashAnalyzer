79c7f8159ab71fbaf95947f706f1c995465c01d0
AlienShuffle <readngtndude@gmail.com>
2025-03-02 20:24:32 -0500

    in process #2

--- meta.pluto.sh
+++ meta.pluto.sh
@@ -1,4 +1,5 @@
 # this is an include file for platform-specific parameters.
 # publish folder home for Google Drive targets.
 publishHome='/mnt/c/Users/alan/My Drive (readngtndude@gmail.com)/CashAnalyzer'
-#echo "meta.pluto.home loading in $0"
+cloudFlareHome="$HOME/cloudflare/public"
+echo "meta.pluto.home loading in $0"
