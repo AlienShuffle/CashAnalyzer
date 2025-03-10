a858a1c4e464a07eb5aa9d6474dd4a13fefeb73e
AlienShuffle <readngtndude@gmail.com>
2025-03-10 13:07:50 -0400

    remove Google Drive copies.

--- USFR/job-USFR-distro-update-v98.sh
+++ USFR/job-USFR-distro-update-v98.sh
@@ -1,10 +1,8 @@
 source ../meta.$(hostname).sh
 jsonNew="USFR-distro-new-v98.json"
-jsonPub="$publishHome/ETFs/USFR/USFR-distros-v98.json"
 jsonFlare="$cloudFlareHome/ETFs/USFR/USFR-distros.json"
 jsonUnique="USFR-distro-new-unique-v98.json"
 #echo jsonNew=$jsonNew
-#echo jsonPub=$jsonPub
 #
 # preamble - test to see how long since this last run occured, skip out if this run is too soon.
 #  - note, if $1 to to this script is not empty, I will run the script regardless, but report the aging status too.
@@ -12,8 +10,8 @@ jsonUnique="USFR-distro-new-unique-v98.json"
 # update these delayHours as appropriate for the data source.
 updateDelayHours=48
 updateDelaySeconds=$(($updateDelayHours * 60 * 60))
-if [ -f "$jsonPub" ] && [ "$(($(date +"%s") - $(stat -c "%Y" "$jsonPub")))" -lt "$updateDelaySeconds" ]; then
-  echo "Published file is not yet $updateDelayHours hours old - $(stat -c '%y' "$jsonPub" | cut -d: -f1,2)"
+if [ -f "$jsonFlare" ] && [ "$(($(date +"%s") - $(stat -c "%Y" "$jsonFlare")))" -lt "$updateDelaySeconds" ]; then
+  echo "Published file is not yet $updateDelayHours hours old - $(stat -c '%y' "$jsonFlare" | cut -d: -f1,2)"
   [ -z "$1" ] && exit 0
 fi
 runDelayHours=12
@@ -36,27 +34,10 @@ if [ ! -s "$jsonNew" ]; then
   exit 1
 fi
 # this should merge the old and new, removing duplicates and keeping newest.
-if [ -s "$jasonPub" ]; then
-  jq -s 'flatten | unique_by(.exDividendDate) | reverse' "$jsonNew" "$jsonPub" >"$jsonUnique"
+if [ -s "$jsonFlare" ]; then
+  jq -s 'flatten | unique_by(.exDividendDate) | reverse' "$jsonNew" "$jsonFlare" >"$jsonUnique"
 else
   cat "$jsonNew" >"$jsonUnique"
-fi
-# publish google Drive files.
-lenNew=$(grep -o returnOfCapital "$jsonNew" | wc -l)
-if [ -s "$jsonPub" ]; then
-  lenPub=$(grep -o returnOfCapital "$jsonPub" | wc -l)
-else
-  lenPub=0
-  echo "USFR distro history file has not been published."
-  dir=$(dirname "$jsonPub")
-  [ -d "$dir" ] || mkdir -p "$dir"
-fi
-echo "entries new($lenNew) :: pub($lenPub)"
-lenUnique=$(grep -o returnOfCapital "$jsonUnique" | wc -l)
-echo "entries unique($lenUnique)"
-if [ $lenUnique -gt $lenPub ]; then
-  cat "$jsonUnique" >"$jsonPub"
-  echo "published updated USFR distro history file."
 fi
 # publish cloudFlare files.
 lenNew=$(grep -o returnOfCapital "$jsonNew" | wc -l)
--- USFR/job-USFR-facts-update-v98.sh
+++ USFR/job-USFR-facts-update-v98.sh
@@ -1,9 +1,7 @@
 source ../meta.$(hostname).sh
 jsonNew=USFR-facts-new-v98.json
-jsonPub="$publishHome/ETFs/USFR/USFR-facts-v98.json"
 jsonFlare="$cloudFlareHome/ETFs/USFR/USFR-facts.json"
 #echo jsonNew=$jsonNew
-#echo jsonPub=$jsonPub
 #
 # preamble - test to see how long since this last run occured, skip out if this run is too soon.
 #  - note, if $1 to to this script is not empty, I will run the script regardless, but report the aging status too.
@@ -11,8 +9,8 @@ jsonFlare="$cloudFlareHome/ETFs/USFR/USFR-facts.json"
 # update these delayHours as appropriate for the data source.
 updateDelayHours=18
 updateDelaySeconds=$(($updateDelayHours * 60 * 60))
-if [ -f "$jsonPub" ] && [ "$(($(date +"%s") - $(stat -c "%Y" "$jsonPub")))" -lt "$updateDelaySeconds" ]; then
-  echo "Published file is not yet $updateDelayHours hours old - $(stat -c '%y' "$jsonPub" | cut -d: -f1,2)"
+if [ -f "$jsonFlare" ] && [ "$(($(date +"%s") - $(stat -c "%Y" "$jsonFlare")))" -lt "$updateDelaySeconds" ]; then
+  echo "Published file is not yet $updateDelayHours hours old - $(stat -c '%y' "$jsonFlare" | cut -d: -f1,2)"
   [ -z "$1" ] && exit 0
 fi
 runDelayHours=4
@@ -44,20 +42,9 @@ if [ -z "$dateNew" ]; then
   exit 1
 fi
 echo dateNew=$dateNew
-if [ -s "$jsonPub" ]; then
-  datePub=$(grep asOfDate "$jsonPub" | cut -d: -f2 | sed 's/\"//g' | sed 's/,//g' | sed 's/ //g')
-else
-  datePub=""
-  echo "USFR facts file has not been published."
-  dir=$(dirname "$jsonPub")
-  [ -d "$dir" ] || mkdir -p "$dir"
-fi
-echo datePub=$datePub
-if [[ $datePub < $dateNew ]]; then
-  cat "$jsonNew" >"$jsonPub"
-  echo "published updated USFR facts file."
-fi
+#
 # cloudFlare files.
+#
 if [ -s "$jsonFlare" ]; then
   dateFlare=$(grep asOfDate "$jsonFlare" | cut -d: -f2 | sed 's/\"//g' | sed 's/,//g' | sed 's/ //g')
 else
