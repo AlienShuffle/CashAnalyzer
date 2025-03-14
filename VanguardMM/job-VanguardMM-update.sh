47d472d8f84e78f23c14effad6b2e3aed8a83dea
AlienShuffle <readngtndude@gmail.com>
2025-03-13 21:39:29 -0400

    first test of VanguardMM

--- VanguardMM/job-VanguardMM-update.sh
+++ VanguardMM/job-VanguardMM-update.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/bash
-bankName=yieldMM
+bankName=$(basename $(pwd))
 queryFile=$bankName-file.json
 
-# go pull the home page off the yieldFinder app.
-curl -sSL https://yieldFinder.app/json | jq . >"$queryFile"
+# go pull the vanguard.com funds month-end reports page off the vanguard.com.
+curl -sSL --referer 'https://investor.vanguard.com' "https://api.vanguard.com/rs/ire/01/ind/mf/month-end.jsonp?" |
+    sed -e 's/^callback(//' |
+    sed -e 's/)$//' |
+    jq '.fund.entity' >"$queryFile"
 if [ ! -s "$queryFile" ]; then
     echo "Empty $queryFile file."
     exit 1
--- VanguardMM/node-VanguardMM-update.js
+++ VanguardMM/node-VanguardMM-update.js
@@ -1,5 +1,11 @@
+const du = require('../lib/dateUtils.js');
 // Work on POSIX and Windows
 const fs = require("fs");
+
+function safeObjectRef(obj) {
+    if (typeof obj === "undefined") return "";
+    return obj;
+}
 
 // read in from stdin, the yieldFinder.app source data json file.
 const stdinBuffer = fs.readFileSync(process.stdin.fd);
@@ -8,17 +14,26 @@ const timestamp = new Date();
 // convert the data into my facts file format and dump on stdout.
 let resp = [];
 for (let i = 0; i < json.length; i++) {
-    const acct = json[i];
-    if (acct.account_type == 'Money Market Fund') {
-        const row = {
-            "ticker": acct.ticker,
-            "source": 'yieldFinder',
-            "timestamp": timestamp,
-            "accountType": acct.name,
-            "sevenDayYield": (acct.rate / 100).toFixed(4) * 1,
-            "asOfDate": acct.rate_as_of,
-        };
-        resp.push(row);
+    const ticker = json[i].profile.ticker;
+    switch (ticker) {
+        case 'VCTXX':
+        case 'VMFXX':
+        case 'VMRXX':
+        case 'VMSXX':
+        case 'VUSXX':
+        case 'VYFXX':
+            const row = {
+                "ticker": ticker,
+                "source": 'Vanguard.com',
+                "timestamp": timestamp,
+                "accountType": safeObjectRef(json[i].profile.shortName),
+                "sevenDayYield": (safeObjectRef(json[i].yield.yieldPct) / 100).toFixed(5) * 1,
+                "asOfDate": du.getISOString(new Date(safeObjectRef(json[i].yield.asOfDate))),
+            };
+            resp.push(row);
+            break;
+        default:
+            break;
     }
 }
 console.log(JSON.stringify(resp));
