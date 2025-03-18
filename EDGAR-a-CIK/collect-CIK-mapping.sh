42e24da6c3ebe0b900a2eed0d12f9635ec3d7f4a
AlienShuffle <readngtndude@gmail.com>
2025-03-18 13:59:00 -0400

    comments

--- EDGAR-a-CIK/collect-CIK-mapping.sh
+++ EDGAR-a-CIK/collect-CIK-mapping.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
 #
-# Pull a today's summary yields report for feeding into the processor.
+# Create a mappings of tickers to EDGAR CIK keys, used to look up more details about each fund.
+# This script takes each fund ticker, looks it up in the EDGAR company_tickers_mf.json file
+# and creates a JSON object that collects all the key data in on place.
 #
 # the current funds list is updated by a script in the mmFunCurr directory.
 #
