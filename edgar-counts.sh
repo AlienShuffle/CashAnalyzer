318140e187c82b6da16a848681360ecee4bbbb77
AlienShuffle <readngtndude@gmail.com>
2025-04-19 12:27:33 -0400

    fix folder references

--- edgar-counts.sh
+++ edgar-counts.sh
@@ -1,13 +1,13 @@
 # count the processing status of the EDGAR data.
 echo N-MFP file references in MFP-List:
-grep url EDGAR-c-MFP-lists/MFP-lists/*.json | cut -d: -f2- | wc -l
+grep url EDGAR-d-MFP-lists/MFP-lists/*.json | cut -d: -f2- | wc -l
 echo number downloaded XML files derived from MFP-lists
-find EDGAR-c-MFP-lists/MFP-files -type f -name '*.xml' -print | wc -l
+find EDGAR-d-MFP-lists/MFP-files -type f -name '*.xml' -print | wc -l
 echo number of parsed XML files into MM reports
-find EDGAR-d-MFP-parse/reports -type f -name '*.json' -print | wc -l
+find EDGAR-e-MFP-parse/reports -type f -name '*.json' -print | wc -l
 echo number of parsed XML files into yield reports
-find EDGAR-d-MFP-parse/yields -type f -name '*.json' -print | wc -l
+find EDGAR-e-MFP-parse/yields -type f -name '*.json' -print | wc -l
 echo number of tickers tracked with MM reports
-find EDGAR-d-MFP-parse/tickers -type f -name '*.json' -print | wc -l
+find EDGAR-e-MFP-parse/tickers -type f -name '*.json' -print | wc -l
 echo number of tickers tracked with yield reports
-find EDGAR-d-MFP-parse/history -type f -name 'rate-new.json'  -print | wc -l
+find EDGAR-e-MFP-parse/history -type f -name 'rate-new.json'  -print | wc -l
