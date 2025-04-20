7b4b8567084a1856d4daeb86c67c3aa9541ef4a4
AlienShuffle <readngtndude@gmail.com>
2025-04-19 21:55:46 -0400

    undo reporting tool, too messy, way too messy!

--- EDGAR-d-MFP-lists/job-download-MFP-files.sh
+++ EDGAR-d-MFP-lists/job-download-MFP-files.sh
@@ -19,9 +19,6 @@ while [ -n "$1" ]; do
         forceRun=true
         #echo "forceRun=$forceRun"
         ;;
-    "--missingReport")
-        missingReport=true
-        ;;
     "--pubDelay")
         pubDelayHours="$2"
         #echo "pubDelayHours=$pubDelayHours"
@@ -42,36 +39,32 @@ done
 # computer-specific configurations.
 source ../meta.$(hostname).sh
 
-getCount=0
 listCount=0
+getCount=0
 for list in $MFPListsDir/*.json; do
     #echo "Processing $list"
-    # this next line is moved to end to not create a sub-shell via piping.
-    #cat "$list" | jq -r '.[] | [.cik,.accessionNumber,.url,.filingDate] | @csv' | sed -e 's/"//g' |
-    while read -r entry; do
-        cik=$(echo $entry | cut -d, -f1)
-        accessionNumber=$(echo $entry | cut -d, -f2)
-        url=$(echo $entry | cut -d, -f3)
-        filingDate=$(echo $entry | cut -d, -f4)
-        #echo "filingDate=$filingDate"
-        targetDir="$MFPFilesDir/$cik"
-        [ -d "$targetDir" ] || mkdir -p "$targetDir"
-        targetFile="$targetDir/$filingDate-$accessionNumber.xml"
-        if [ ! -s "$targetFile" ]; then
-            echo "$getCount: download $cik : $accessionNumber"
-            if [ -z "$missingReport" ]; then
+    cat "$list" | jq -r '.[] | [.cik,.accessionNumber,.url,.filingDate] | @csv' | sed -e 's/"//g' |
+        while read -r entry; do
+            cik=$(echo $entry | cut -d, -f1)
+            accessionNumber=$(echo $entry | cut -d, -f2)
+            url=$(echo $entry | cut -d, -f3)
+            filingDate=$(echo $entry | cut -d, -f4)
+            #echo "filingDate=$filingDate"
+            targetDir="$MFPFilesDir/$cik"
+            [ -d "$targetDir" ] || mkdir -p "$targetDir"
+            targetFile="$targetDir/$filingDate-$accessionNumber.xml"
+            if [ ! -s "$targetFile" ]; then
+                echo "$getCount: download $cik : $accessionNumber"
                 ../bin/getEDGAR.sh "$url" >"$targetFile"
                 sleep 1
                 if grep '<title>SEC.gov | File Unavailable</title>' $targetFile; then
                     echo "failed download: $targetFile"
                     rm $targetFile
                 fi
+                getCount=$(($getCount + 1))
             fi
-            getCount=$(($getCount + 1))
-        fi
-        [ -z "$missingReport" ] && [ $getCount -gt 19 ] && exit 1
-    done < <(cat "$list" | jq -r '.[] | [.cik,.accessionNumber,.url,.filingDate] | @csv' | sed -e 's/"//g')
+            [ $getCount -gt 19 ] && exit 1
+        done
     listCount=$(($listCount + 1))
 done
-[ ! -z "$missingReport" ] && echo "$listCount CIKs, $getCount files to be downloaded."
 exit 0
