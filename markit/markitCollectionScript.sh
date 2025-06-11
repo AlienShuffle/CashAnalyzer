22e1c311c83dee63ee78adaad2b9f9100f023328
AlienShuffle <readngtndude@gmail.com>
2025-06-10 22:01:00 -0400

    doc cleanup

--- markit/markitCollectionScript.sh
+++ markit/markitCollectionScript.sh
@@ -10,26 +10,27 @@ token=$(curl --header 'Content-Type: application/json' \
     -sSL "https://www.reuters.com/service/graphql" \
     --data @token-query.json |
     jq -r '.data.getModToken.accessToken')
+#echo $token
+#exit 0
+firstRow=true
+echo "{"
+# note, this cut is reading from stdin and expects a csv list of fund ticker, ticker, names to drive the queries.
+while IFS= read -r ticker; do
+    [ "$firstRow" = "false" ] && echo ","
+    echo "\"${ticker}\": "
 
-(
-    firstRow=true
-    echo "{"
-    # note, this cut is reading from stdin and expects a csv list of fund ticker, ticker, names to drive the queries.
-    while IFS= read -r ticker; do
-        [ "$firstRow" = "false" ] && echo ","
-        echo "\"${ticker}\": "
+    xid=$(curl -sSL \
+        "https://content.markitcdn.com/api.markitondemand.com/apiman-gateway/MOD/chartworks-xref/1.0/xref/exact?inputs=%5B%7B%22symbol%22%3A%22${ticker}.O%22%7D%5D&access_token=${token}" |
+        jq -r '.data.items[0].xid')
 
-        xid=$(curl -sSL \
-            "https://content.markitcdn.com/api.markitondemand.com/apiman-gateway/MOD/chartworks-xref/1.0/xref/exact?inputs=%5B%7B%22symbol%22%3A%22${ticker}.O%22%7D%5D&access_token=${token}" |
-            jq -r '.data.items[0].xid')
-
-        sed -s "s/XXxidXX/${xid}/" ./content-template.json >content-xid.json
-        curl -sSL "https://content.markitcdn.com/api.markitondemand.com/apiman-gateway/MOD/chartworks-data/1.0/chartapi/series?access_token=${token}" \
-            --header "$curlAgentHeader" \
-            --header 'Content-Type: application/json' \
-            --data @content-xid.json | node ./node-parse-markit-file.js
-        firstRow=false
-    done
-    echo "}"
-)
+    sed -s "s/XXxidXX/${xid}/" ./content-template.json >content-xid.json
+    curl -sSL "https://content.markitcdn.com/api.markitondemand.com/apiman-gateway/MOD/chartworks-data/1.0/chartapi/series?access_token=${token}" \
+        --header "$curlAgentHeader" \
+        --header 'Content-Type: application/json' \
+        --data @content-xid.json |
+        #tee $ticker.json |
+        node ./node-parse-markit-file.js
+    firstRow=false
+done
+echo "}"
 rm -f content-xid.json
