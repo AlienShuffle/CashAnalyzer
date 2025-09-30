67820a5ab21703c2fcf9328695ae24f977406c64
AlienShuffle <readngtndude@gmail.com>
2025-09-30 10:46:19 -0400

    vanguard price updates fixed!

--- VanguardPrices/inst-collectionScript.sh
+++ VanguardPrices/inst-collectionScript.sh
@@ -6,21 +6,21 @@ if [ -n "$1" ]; then
     beginDate=$1'-01-01'
     endDate=$1'-12-31'
 else
-    beginDate=$(date +'%Y-01-01')
+    beginDate=$(date -d "30 days ago" +'%Y-%m-%d')
     endDate=$(date +'%Y-%m-%d')
 fi
 (
     firstRow=true
     echo "["
     while IFS= read -r row; do
-       [ "$firstRow" = "false" ] && echo ","
-       fundId=$(echo $row | cut -d, -f2)
-       ticker=$(echo $row | cut -d, -f1)
-       echo $ticker 1>&2
-       url="https://institutional.vanguard.com/investments/yieldsServiceProxy?portIds=$(echo $fundId)&timePeriodCode=D&effectiveDate=$beginDate:to:$endDate&yieldCodes=1DISTYLD,CMPNDYLDPC,SEC,7DISTYLD,30DISTYLD"
-
-      curl -sSL $url > tmp.json
-        sed -e 's/\"yields\":{/\"ticker\":\"'$ticker'\", \"yields\": {/g' < tmp.json
+        [ "$firstRow" = "false" ] && echo ","
+        fundId=$(echo $row | cut -d, -f2)
+        ticker=$(echo $row | cut -d, -f1)
+        #echo $ticker 1>&2
+        url="https://institutional.vanguard.com/investments/yieldsServiceProxy?portIds=$(echo $fundId)&timePeriodCode=D&effectiveDate=$beginDate:to:$endDate&yieldCodes=1DISTYLD,CMPNDYLDPC,SEC,7DISTYLD,30DISTYLD"
+        #echo $url 1>&2
+        curl -sSL $url |
+            sed -e 's/\"yields\":{/\"ticker\":\"'$ticker'\", \"yields\": {/g'
         firstRow=false
     done
     echo "]"
--- VanguardPrices/node-inst-VanguardPrices-update.js
+++ VanguardPrices/node-inst-VanguardPrices-update.js
@@ -5,8 +5,8 @@ const fs = require("fs");
 require('../lib/dynamicSort.js')();
 
 function safeObjectRef(obj) {
-    if (typeof obj === "undefined") return "";
-    return obj;
+  if (typeof obj === "undefined") return "";
+  return obj;
 }
 
 // read in from stdin, the Vanguard price history tool source data rows file.
@@ -16,51 +16,51 @@ const timestamp = new Date();
 let resp = [];
 // go through each ticker report provided.
 for (let i = 0; i < json.length; i++) {
-    const ticker = json[i].ticker;
-    if (!ticker) continue;
+  const ticker = json[i].ticker;
+  if (!ticker) continue;
 
-    const yields = json[i].yields.content[0].yields;
-    const numRows = yields.length;
+  const yields = json[i].yields.content[0].yields;
+  const numRows = yields.length;
 
-    let buffer = {};
-    for (let j = 0; j < numRows; j++) {
-        const yield = yields[j];
-        if (yield.timePeriodCode != 'D') continue;
-        const effectiveDate = yield.effectiveDate;
-        if (!buffer[effectiveDate]) {
-            buffer[effectiveDate] = {
-                "oneDayYield": null,
-                "sevenDayYield": null,
-                "thirtyDayYield": null,
-            };
-        }
-        switch (yield.yieldCode) {
-            case '1DISTYLD':
-                buffer[effectiveDate].oneDayYield = (parseFloat(yield.percent) / 100).toFixed(6) * 1;
-                break;
-            case 'SEC':
-                buffer[effectiveDate].sevenDayYield = (parseFloat(yield.percent) / 100).toFixed(6) * 1;
-                break;
-            case '30DISTYLD':
-                buffer[effectiveDate].thirtyDayYield = (parseFloat(yield.percent) / 100).toFixed(6) * 1;
-                break;
-            default:
-                break;
-        }
+  let buffer = {};
+  for (let j = 0; j < numRows; j++) {
+    const yield = yields[j];
+    if (yield.timePeriodCode != 'D') continue;
+    const effectiveDate = yield.effectiveDate;
+    if (!buffer[effectiveDate]) {
+      buffer[effectiveDate] = {
+        "oneDayYield": null,
+        "sevenDayYield": null,
+        "thirtyDayYield": null,
+      };
     }
-    for (const effectiveDate of Object.keys(buffer)) {
-        if (!safeObjectRef(buffer[effectiveDate])) continue;
-        if (!safeObjectRef(buffer[effectiveDate].sevenDayYield)) continue;
-        let tickerData = {};
-        tickerData.asOfDate = effectiveDate;
-        tickerData.price = 1;
-        if (safeObjectRef(buffer[effectiveDate].oneDayYield)) tickerData.oneDayYield = buffer[effectiveDate].oneDayYield;
-        tickerData.sevenDayYield = buffer[effectiveDate].sevenDayYield;
-        if (safeObjectRef(buffer[effectiveDate].thirtyDayYield)) tickerData.thirtyDayYield = buffer[effectiveDate].thirtyDayYield;
-        tickerData.source = 'Vanguard.com Inst Price History';
-        tickerData.ticker = ticker;
-        tickerData.timestamp = timestamp;
-        resp.push(tickerData);
+    switch (yield.yieldCode) {
+      case '1DISTYLD':
+        buffer[effectiveDate].oneDayYield = (parseFloat(yield.percent) / 100).toFixed(6) * 1;
+        break;
+      case 'SEC':
+        buffer[effectiveDate].sevenDayYield = (parseFloat(yield.percent) / 100).toFixed(6) * 1;
+        break;
+      case '30DISTYLD':
+        buffer[effectiveDate].thirtyDayYield = (parseFloat(yield.percent) / 100).toFixed(6) * 1;
+        break;
+      default:
+        break;
     }
+  }
+  for (const effectiveDate of Object.keys(buffer)) {
+    if (!safeObjectRef(buffer[effectiveDate])) continue;
+    if (!safeObjectRef(buffer[effectiveDate].sevenDayYield)) continue;
+    let tickerData = {};
+    tickerData.asOfDate = effectiveDate;
+    tickerData.price = 1;
+    if (safeObjectRef(buffer[effectiveDate].oneDayYield)) tickerData.oneDayYield = buffer[effectiveDate].oneDayYield;
+    tickerData.sevenDayYield = buffer[effectiveDate].sevenDayYield;
+    if (safeObjectRef(buffer[effectiveDate].thirtyDayYield)) tickerData.thirtyDayYield = buffer[effectiveDate].thirtyDayYield;
+    tickerData.source = 'Vanguard.com Inst Price History';
+    tickerData.ticker = ticker;
+    tickerData.timestamp = timestamp;
+    resp.push(tickerData);
+  }
 }
 console.log(JSON.stringify(resp.sort(dynamicSort('ticker', 'asOfDate'))));
