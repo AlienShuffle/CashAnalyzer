0d6decd13ab08446e8cf0806f168117bc5157d4a
AlienShuffle <readngtndude@gmail.com>
2025-09-30 10:49:03 -0400

    removed old price history tool code, just use the new institutional version.

--- VanguardPrices/paused-old-VanguardPrices-update.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-cat "$(basename $(pwd))-funds.csv" |
-    ../bin/MM-update-common-job.sh \
-        --collectionScript ./collectionScript.sh \
-        --processScript ./node-VanguardPrices-update.js \
-        --pubDelay 20 --runDelay 2 "$@"
