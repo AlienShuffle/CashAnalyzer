f37da6ff70d612d21aad4a40be0d2c3b8f553d97
AlienShuffle <readngtndude@gmail.com>
2025-10-02 21:06:20 -0400

    turned off the older MM scrapes

--- VanguardPrices/job-VanguardPrices-update.sh
+++ VanguardPrices/job-VanguardPrices-update.sh
@@ -3,4 +3,4 @@ cat "$(basename $(pwd))-funds.csv" |
     ../bin/MM-update-common-job.sh \
         --collectionScript ./inst-collectionScript.sh \
         --processScript ./node-inst-VanguardPrices-update.js \
-        --pubDelay 20 --runDelay 2 "$@"
+        --pubDelay 8 --runDelay 2 "$@"
