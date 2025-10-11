1c06bf990a7672363437dd29fba0e8f49243cf83
AlienShuffle <readngtndude@gmail.com>
2025-10-11 17:28:15 -0400

    re-instated yieldFinder

--- yieldFinder/node-yieldFinder-update.js
+++ yieldFinder/node-yieldFinder-update.js
@@ -2,18 +2,33 @@
 const fs = require("fs");
 
 // read in from stdin, the yieldFinder.app source data json file.
+/** Sample
+ * {
+  "last_updated": "2025-10-11T21:12:04.827Z",
+  "data": [
+    {
+      "date": "2025-10-11",
+      "account_type": "Savings Account",
+      "institution_name": "Ever Bank",
+      "account_name": "Ever Bank",
+      "value": 4.05
+    }
+  ]
+}
+ */
 const stdinBuffer = fs.readFileSync(0, 'utf-8');
 const json = JSON.parse(stdinBuffer);
+const data = json.data;
 const timestamp = new Date();
 // convert the data into my facts file format and dump on stdout.
 let resp = [];
-for (let i = 0; i < json.length; i++) {
-    const acct = json[i];
+for (let i = 0; i < data.length; i++) {
+    const acct = data[i];
     if (acct.account_type == 'Savings Account') {
         const row = {
-            "accountType": acct.name,
-            "apy": (acct.rate / 100).toFixed(4) * 1,
-            "asOfDate": acct.rate_as_of,
+            "accountType": acct.account_name,
+            "apy": (acct.value / 100).toFixed(4) * 1,
+            "asOfDate": acct.date,
             "source": 'node-yieldFinder-json-update.js',
             "timestamp": timestamp,
         };
