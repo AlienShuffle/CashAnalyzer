224c2a0c6d2b65697990580e97eda6c6d4029700
AlienShuffle <readngtndude@gmail.com>
2025-11-27 22:38:16 -0500

    tday last save

--- lwlots/node-lot-retrieval.js
+++ lwlots/node-lot-retrieval.js
@@ -1,18 +1,65 @@
+import dynamicSort from '../lib/dynamicSort.mjs'
 import { parse } from 'node-html-parser';
 import { readFileSync } from 'fs';
 import process from 'node:process';
 
+// import filter list for parcel prefixes.
+
+const prefixesString = readFileSync(process.argv[2], 'utf8');
+const prefixes = prefixesString
+    .split('\n')
+    .map(line => line.trim())
+    .filter(line => line.length > 0);
+function isValidParcel(parcelString) {
+    for (const prefix of prefixes) {
+        if (parcelString.startsWith(prefix)) {
+            return true;
+        }
+    }
+    return false;
+}
 const htmlString = readFileSync(process.stdin.fd, 'utf8');
-
 const root = parse(htmlString);
-
 const lotListElement = root.querySelector('#list');
 const childCnt = lotListElement.children.length;
-console.log(`childNodes.length = ${childCnt.length}`);
+//console.error(`childNodes.length = ${childCnt}`);
+
+const oldDate = new Date('2000-01-01T00:00:00Z');
+
+const result = [];
 for (let i = 0; i < childCnt; i++) {
 
-    const lotRow = lotListElement.firstElementChild;
-    console.log(`href = ${lotRow.querySelector('href')}`)
+    const row = lotListElement.children[i];
+    //console.error(row.structure);
 
-    //console.log('Title:', title);
+    const parcelString = row.textContent
+        .replace(/^.*Mblu:/, '')
+        .replace(/[\s\n\r]/g, '')
+        .trim()
+        .replace(/\/$/, '')
+        .replace(/\//g, '-')
+
+    if (!isValidParcel(parcelString)) {
+        console.error(`Skipping invalid parcel: ${parcelString}`);
+        continue;
+    }
+
+    const lot = parcelString.split('-')[2];
+    const linkElement = row.children[0];
+
+    const addressString = linkElement.text;
+
+    const pidString = linkElement.getAttribute('href')
+        .replace(/Parcel.aspx\?pid=/, '');
+
+    result.push({
+        lot: lot*1,
+        address: addressString,
+        pid: pidString*1,
+        parcel: parcelString,
+        updateDate: oldDate,
+        queryDate: oldDate
+    })
 }
+
+console.log(JSON.stringify(result.sort(dynamicSort('lot')), null, 2));
--- lwlots/retrieve-lots.sh
+++ lwlots/retrieve-lots.sh
@@ -2,6 +2,7 @@
 #
 # Pull all the lots using a list of streets as the driver to collect them. uses parcel prefixes to filter out non LWPOA lots.
 #
+tmpFile=street.html
 (
     count=0
     firstRow=true
@@ -10,14 +11,16 @@
         while IFS= read -r row; do
             [ "$firstRow" = "false" ] && echo ","
             street=$(echo $row | sed -e 's/ /%20/g')
-            echo $street 1>&2
+            echo $row 1>&2
             url="https://gis.vgsi.com/SchuylkillCountyPA/Streets.aspx?Name=$street"
-            #echo $url 1>&2
-            curl -ksSL "$url" # |
-            #    node ./node-retrievel.js parcel-prefix.csv
+            echo $url 1>&2
+            curl -ksSL "$url" >"$tmpFile"
+            cat $tmpFile | node ./node-lot-retrieval.js parcel-prefix.csv
             firstRow=false
             count=$(($count + 1))
-            [ $count -ge 1 ] && break
+            [ $count -ge 500 ] && break
         done
     echo "]"
-) #| jq .
+) > lots.tmp.json
+cat lots.tmp.json | jq -s 'flatten' > lots.json
+rm -f $tmpFile
