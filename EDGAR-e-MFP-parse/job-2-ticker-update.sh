0c5aa993d17f8e6263d416e0e25cfa1b55fa798c
AlienShuffle <readngtndude@gmail.com>
2025-12-21 22:05:39 -0500

    npm update, empty row error in company liset,

--- EDGAR-e-MFP-parse/job-2-ticker-update.sh
+++ EDGAR-e-MFP-parse/job-2-ticker-update.sh
@@ -50,6 +50,7 @@ fi
 
 cat $fundsList |
     while read -r ticker; do
+        [ -z "$ticker" ] || continue
         cik=$(node ./node-ticker-CIK-map.js "$ticker" <"$fundsMetaFile")
         if [ ! -n "$cik" ]; then
             echo "$ticker: missing from company-map, skipping...."
