597edc64aa989a11c8a37f10b12211c1e93ca87c
AlienShuffle (dev@dev) <readngtndude@gmail.com>
2026-03-27 15:25:29 -0400

    cleanup.

--- meta.pluto.sh
+++
@@ -1,6 +0,0 @@
-[ -f ../meta.common.sh ] && source ../meta.common.sh
-# this is an include file for platform-specific parameters.
-# publish folder home for Google Drive targets.
-publishHome='/mnt/c/Users/alan/My Drive (readngtndude@gmail.com)/CashAnalyzer'
-cloudFlareHome="$HOME/cloudflare/public"
-#echo "meta.pluto.home loading in $0"
