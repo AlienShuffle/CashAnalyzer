7730f970208d60f5441c78649274f8fea2731733
AlienShuffle (dev@dev) <readngtndude@gmail.com>
2026-05-09 21:31:17 -0400

    improved diffing of json files.

--- lwlots/compare-runs.sh
+++ lwlots/compare-runs.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/bash
 
-previousReportDir="$1"
-latestReportDir="$2"
+[ "$1" == "" ] || [ "$2" == "" ] && {
+    echo "Usage: $0 [--ignoreTaxes] <previousReportDir> <latestReportDir>"
+    exit 1
+}
+if [ "$1" == "--ignoreTaxes" ]; then
+    ignoreTaxes=true
+    previousReportDir="$2"
+    latestReportDir="$3"
+else
+    ignoreTaxes=false
+    previousReportDir="$1"
+    latestReportDir="$2"
+fi
 
 [ -f "$previousReportDir/1-lot-list.csv" ] || {
     echo "$previousReportDir/1-lot-list.csv: Report file not found. Please run the report generation script first."
@@ -17,7 +28,7 @@ for i in $(basename -a $latestReportDir/*.json $latestReportDir/*.csv | cut -d'.
 
     if [ -f "$previousReportDir/$csvBaseName" ]; then
         echo -n "$csvBaseName: "
-        diff "$previousReportDir/$csvBaseName" "$latestReportDir/$csvBaseName"
+        diff "$previousReportDir/$csvBaseName" "$latestReportDir/$csvBaseName" >/dev/null
         if [ $? -eq 0 ]; then
             echo "identical"
         else
@@ -32,15 +43,53 @@ for i in $(basename -a $latestReportDir/*.json $latestReportDir/*.csv | cut -d'.
         ../bin/jsonDifferent.sh "$previousReportDir/$jsonBaseName" "$latestReportDir/$jsonBaseName"
         if [ $? -eq 0 ]; then
             echo -e "different"
-            switch $jsonBaseName in
-                "3-lot-taxes.json")
+            case "$jsonBaseName" in
+            "1-lot-list.json")
+                jd \
+                    <(jq 'map(del(.timestamp)) | map({ ((.lot | tostring) + " (" + .location + ")"): . }) | add' "$previousReportDir/$jsonBaseName") \
+                    <(jq 'map(del(.timestamp)) | map({ ((.lot | tostring) + " (" + .location + ")"): . }) | add' "$latestReportDir/$jsonBaseName")
+                ;;
+            "3-lot-taxes.json")
+                if [ "$ignoreTaxes" = true ]; then
                     jd \
-                      <(jq 'map(del(.timestamp)) | map({ ((.lot | tostring) + " (" + .owners + ")"): . }) | add' "$previousReportDir/$jsonBaseName") \
-                      <(jq 'map(del(.timestamp)) | map({ ((.lot | tostring) + " (" + .owners + ")"): . }) | add' "$latestReportDir/$jsonBaseName")
-                    ;;
-                *)
-                    echo "No specific diff command for $jsonBaseName. Please check the files manually."
-                    ;;
+                        <(jq 'map(del(.status.historicalDelinquency) | del(.status.taxesDue) | del(.timestamp)) | map({ ((.lot | tostring) + " (" + .owners + ")"): . }) | add' "$previousReportDir/$jsonBaseName") \
+                        <(jq 'map(del(.status.historicalDelinquency) | del(.status.taxesDue) | del(.timestamp)) | map({ ((.lot | tostring) + " (" + .owners + ")"): . }) | add' "$latestReportDir/$jsonBaseName")
+                else
+                    jd \
+                        <(jq 'map(del(.timestamp)) | map({ ((.lot | tostring) + " (" + .owners + ")"): . }) | add' "$previousReportDir/$jsonBaseName") \
+                        <(jq 'map(del(.timestamp)) | map({ ((.lot | tostring) + " (" + .owners + ")"): . }) | add' "$latestReportDir/$jsonBaseName")
+                fi
+                ;;
+            "4-lot-normalized.json")
+                jd \
+                    <(jq 'map(del(.timestamp)) | map({ ((.lot | tostring) + " (" + .generalOwner + ")"): . }) | add' "$previousReportDir/$jsonBaseName") \
+                    <(jq 'map(del(.timestamp)) | map({ ((.lot | tostring) + " (" + .generalOwner + ")"): . }) | add' "$latestReportDir/$jsonBaseName")
+                ;;
+            "5-owner-list.json")
+                jd \
+                    <(jq 'map(del(.timestamp)) | map({ (.owner): . }) | add' "$previousReportDir/$jsonBaseName") \
+                    <(jq 'map(del(.timestamp)) | map({ (.owner): . }) | add' "$latestReportDir/$jsonBaseName")
+                ;;
+            "6-addr-list.json")
+                jd \
+                    <(jq 'map(del(.timestamp)) | map({ (.address): . }) | add' "$previousReportDir/$jsonBaseName") \
+                    <(jq 'map(del(.timestamp)) | map({ (.address): . }) | add' "$latestReportDir/$jsonBaseName")
+                ;;
+
+            "8-filtered-report.json")
+            if [ "$ignoreTaxes" = true ]; then 
+                jd \
+                    <(jq 'map(del(.taxStatus.historicalDelinquency) | del(.taxStatus.taxesDue) | del(.timestamp)) | map({ ((.lot | tostring) + " (" + .generalOwner + ")"): . }) | add' "$previousReportDir/$jsonBaseName") \
+                    <(jq 'map(del(.taxStatus.historicalDelinquency) | del(.taxStatus.taxesDue) | del(.timestamp)) | map({ ((.lot | tostring) + " (" + .generalOwner + ")"): . }) | add' "$latestReportDir/$jsonBaseName")
+                else
+                    jd \
+                        <(jq 'map(del(.timestamp)) | map({ ((.lot | tostring) + " (" + .generalOwner + ")"): . }) | add' "$previousReportDir/$jsonBaseName") \
+                        <(jq 'map(del(.timestamp)) | map({ ((.lot | tostring) + " (" + .generalOwner + ")"): . }) | add' "$latestReportDir/$jsonBaseName")
+                fi
+                ;;
+            *)
+                echo "No specific diff command for $jsonBaseName. Please check the files manually."
+                ;;
             esac
         else
             echo -e "identical"
--- lwlots/run-b-compare.sh
+++ lwlots/run-b-compare.sh
@@ -1,17 +1,30 @@
 #!/usr/bin/bash
-[ -f history/report-previous.txt ] || {
-    echo "report-previous.txt report file not found. Wait a day and run report generation script."
-    exit 1
-}
-[ -f history/report-latest.txt ] || {
+
+if [ ! -f history/report-latest.txt ]; then
     echo "report-latest.txt report file not found. Please run the report generation script first."
     exit 1
-}
-
-echo comparing reports from history/$(cat history/report-previous.txt) and history/$(cat history/report-latest.txt)
-
-previousReportDir="history/$(cat history/report-previous.txt)"
+fi
 latestReportDir="history/$(cat history/report-latest.txt)"
+
+[ "$1" == "--ignoreTaxes" ] && {
+    ignoreTaxes="--ignoreTaxes"
+    shift
+}
+if [ -n "$1" ]; then
+    previousReportDir="history/$1"
+else
+    if [ ! -f history/report-previous.txt ]; then
+        echo "report-previous.txt report file not found. Wait a day and run report generation script."
+        exit 1
+    fi
+    previousReportDir="history/$(cat history/report-previous.txt)"
+fi
+if [ ! -d "$previousReportDir" ]; then
+    echo "$previousReportDir report directory not found, exit."
+    exit 1
+fi
+
+echo "comparing reports from $previousReportDir and $latestReportDir"
 
 [ -f "$previousReportDir/1-lot-list.csv" ] || {
     echo "$previousReportDir/1-lot-list.csv: file not found. Please run the report generation script first."
@@ -22,4 +35,5 @@ latestReportDir="history/$(cat history/report-latest.txt)"
     exit 1
 }
 
-./compare-runs.sh history/$(cat history/report-previous.txt) history/$(cat history/report-latest.txt)
+./compare-runs.sh $ignoreTaxes $previousReportDir $latestReportDir >b-compare.txt
+cp b-compare.txt "$latestReportDir/b-compare-$(basename $previousReportDir).txt"
