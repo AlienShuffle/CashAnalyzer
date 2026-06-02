cef02d32a17dacc47699b66e9ecb98002c881672
AlienShuffle (production@production) <readngtndude@gmail.com>
2026-06-01 23:05:18 -0400

    quotes not good with ~

--- delete-funds.sh
+++ delete-funds.sh
@@ -5,10 +5,10 @@
 
 for i in BASOX BBSOX BISOX; do
     echo "Deleting $i from BlackRock universe."
-    rm -ir "./BlackRock/history/$i"
-    rm -i "./EDGAR-e-MFP-parse/tickers/$i.json"
-    rm -i "./EDGAR-e-MFP-parse/tax/$i-taxation.json"
-    rm -ir "~/cloudflare/public/MM/$i"
-    rm -ir "~/cloudflare/public/EDGAR/$i"
-    rm -ir "~/cloudflare/public/Funds/$i"
+    rm -ir ./BlackRock/history/$i
+    rm -i ./EDGAR-e-MFP-parse/tickers/$i.json
+    rm -i ./EDGAR-e-MFP-parse/tax/$i-taxation.json
+    rm -ir ~/cloudflare/public/MM/$i
+    rm -ir ~/cloudflare/public/EDGAR/$i
+    rm -ir ~/cloudflare/public/Funds/$i
 done
