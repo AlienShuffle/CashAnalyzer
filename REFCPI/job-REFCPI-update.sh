4a57b4f82835aadfefbc1985acebcd16fec49038
AlienShuffle (dev@dev) <readngtndude@gmail.com>
2026-06-06 09:19:17 -0400

    add REFCPI calculations

---
+++ REFCPI/job-REFCPI-update.sh
@@ -0,0 +1,96 @@
+#!/usr/bin/bash
+#
+# This tool now updates the REFCPI data on the cloudflare site.
+#
+# process the command argument list.
+pubDelayHours=168
+runDelayHours=4
+while [ -n "$1" ]; do
+    case $1 in
+    "-f")
+        forceRun=true
+        #echo "forceRun=$forceRun"
+        ;;
+    "-stdin")
+        stdInFile="$2"
+        #echo "stdInFile=$stdInFile"
+        ;;
+    "-nodearg")
+        nodeArg="$2"
+        #echo "nodeArg=$nodeArg"
+        shift
+        ;;
+    "-pubdelay")
+        pubDelayHours="$2"
+        #echo "pubDelayHours=$pubDelayHours"
+        shift
+        ;;
+    "-rundelay")
+        runDelayHours="$2"
+        #echo "runDelayHours=$runDelayHours"
+        shift
+        ;;
+    "--sourceName")
+        sourceName="$2"
+        #echo "sourceName=$sourceName"
+        shift
+        ;;
+    esac
+    shift
+done
+if [ -z "$sourceName" ]; then
+    sourceName=$(basename $(pwd))
+fi
+if [ ! -d "$HOME/CashAnalyzer/$sourceName" ]; then
+    echo "$0: $sourceName is not a valid CPI script name."
+    exit 1
+fi
+# look for a -f to force run, overriding the time delays.
+
+source ../meta.common.sh
+# current rate files
+cvsNew="$sourceName-new.csv"
+cvsFlare="$cloudFlareHome/Treasuries/$sourceName.csv"
+#
+# preamble - test to see how long since this last run occured, skip out if this run is too soon.
+#  - note, if -f is passed to this script, I will run the script regardless, but report the aging status too.
+#
+# update the delayHours values as appropriate for the data source.
+pubDelayFile="$cvsFlare"
+runDelayFile="$cvsNew"
+source ../bin/testDelays.sh
+#
+# this script was used in fintools version 98 and later. This is intended to stick around long-term.
+#
+scriptFile="./node-$sourceName-update.js"
+if [ ! -s "$scriptFile" ]; then
+    echo "Missing $scriptFile file."
+    exit 1
+fi
+node $scriptFile "$nodeArg" >"$cvsNew"
+if [ ! $? ]; then
+    echo "$sourceName rate retrieval failed, exiting."
+    exit 1
+fi
+if [ ! -s "$cvsNew" ]; then
+    echo "Empty $sourceName file."
+    exit 1
+fi
+#
+# publish cloudFlare file
+#
+if [ ! -s "$cvsFlare" ]; then
+    echo "$sourceName cloudFlare file had not been published."
+    dir=$(dirname "$cvsFlare")
+    [ -d "$dir" ] || mkdir -p "$dir"
+    cat "$cvsNew" >"$cvsFlare"
+    echo "published updated $sourceName cloudFlare file."
+else
+    if diff "$cvsNew" "$cvsFlare" >/dev/null; then
+        echo "$sourceName file has not changed since last publish."
+    else
+        cat "$cvsNew" >"$cvsFlare"
+        echo "published updated $sourceName cloudFlare file."
+    fi
+fi
+exit 0
---
+++ REFCPI/node-REFCPI-update.js
@@ -0,0 +1,67 @@
+import {
+    duDaysBetween,
+    duGetDateFromYYYYMMDD
+} from '../lib/dateUtils.mjs';
+
+// start in 1996 as that is the year before TIPS were introduced.
+const series = "CPIAUCNS";
+const startDate = "1996-01-01";
+const response = await fetch(`https://fred.stlouisfed.org/graph/fredgraph.csv?id=${series}&cosd=${startDate}&coed=9999-12-31`);
+const csvText = await response.text();
+const monthsText = csvText.split("\n"); 0
+
+// seed with the missing month due to 2025 Govt shutdown, then loop through the rest of the months and build the response for each month.
+let months = [{
+    month: "2025-10-01",
+    CPI: 325.604
+}];
+for (let i = 1; i < monthsText.length; i++) {
+    const row = monthsText[i].split(",");
+    const month = row[0];
+    const CPI = row[1] * 1;
+    if (isNaN(CPI) || CPI === 0) continue; // skip rows with missing CPI values
+    months.push({
+        month: month,
+        CPI: CPI,
+    });
+}
+months.sort((a, b) => new Date(a.month) - new Date(b.month)); // sort by month to get shutdown month in correct order
+
+let resp = [];
+for (let i = 0; i < months.length - 1; i++) {
+    const month = months[i].month;
+    const nextMonth = months[i + 1].month;
+    const refCPI = months[i].CPI;
+    const nextRefCPI = months[i + 1].CPI;
+
+    // add 3 months to get the TIPS month that the CPI value applies to, as REFCPI is calculated with a 3 month lag. 
+    const refCpiMonth = duGetDateFromYYYYMMDD(month);
+    refCpiMonth.setMonth(refCpiMonth.getMonth() + 3);
+    const nextRefCpiMonth = duGetDateFromYYYYMMDD(nextMonth);
+    nextRefCpiMonth.setMonth(nextRefCpiMonth.getMonth() + 3);
+
+    const monthDays = duDaysBetween(refCpiMonth, nextRefCpiMonth);
+    const dailyCPIIncrement = ((nextRefCPI - refCPI) / monthDays).toFixed(6) * 1;
+
+    for (let j = 0; j < monthDays; j++) {
+        const dailyCPIDate = new Date(refCpiMonth);
+        dailyCPIDate.setDate(dailyCPIDate.getDate() + j);
+        resp.push({
+            refCPIDate: dailyCPIDate.toISOString().substring(0, 10),
+            refCPI: refCPI + dailyCPIIncrement * j,
+        });
+    }
+}
+// add first day of last month with the last month's CPI value, as that is the last day that the last month's CPI value applies to.
+const lastDate = duGetDateFromYYYYMMDD(months[months.length - 1].month);
+lastDate.setMonth(lastDate.getMonth() + 3);
+resp.push({
+    refCPIDate: lastDate.toISOString().substring(0, 10),
+    refCPI: months[months.length - 1].CPI,
+});
+// output the .csv content for use in REFCPI.csv
+console.log(`date,REFCPI`);
+for (let i = 0; i < resp.length; i++) {
+    const r = resp[i];
+    console.log(`${r.refCPIDate},${r.refCPI.toFixed(6)}`);
+}
