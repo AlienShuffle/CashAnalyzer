8f944a27786f4458b0471ff0920c897c495d8168
AlienShuffle (dev@dev) <readngtndude@gmail.com>
2026-08-14 22:07:06 -0400

    alllow for configuring sleep time with --sleep parameter.

--- job-control.sh
+++ job-control.sh
@@ -1,6 +1,23 @@
 #!/usr/bin/bash
 cd $HOME/CashAnalyzer
 [ -d $HOME/CashAnalyzer/log ] || mkdir -p $HOME/CashAnalyzer/log
+# set sleep parameter default to 60 minutes or use argument if provided as a parameter --sleep
+# (not passed on to the scripts)
+while [ -n "$1" ]; do
+  case $1 in
+  "--sleep")
+    SLEEP_TIME="$2"
+    echo "SLEEP_TIME=$SLEEP_TIME"
+    shift 2
+    ;;
+  *)
+    # Stop processing - pass remaining args to child processes
+    break
+    ;;
+  esac
+done
+SLEEP_TIME=${SLEEP_TIME:-60m}
+
 # Find all the scripts in each system sub-folder with prefix job- and run them with a standard delay
 while true; do
   logrotate --state $HOME/CashAnalyzer/log/status $HOME/CashAnalyzer/cashanalyzer.log.conf
@@ -16,9 +33,9 @@ while true; do
       )
       echo
     done
-    echo "sleep 90 minutes @ $(date)"
+    echo "sleep $SLEEP_TIME @ $(date)"
   ) | tee -a log/cash-analyzer-jobs.log
-  sleep 90m
+  sleep $SLEEP_TIME
   echo '#####################################################################################'
 done
 exit 0
